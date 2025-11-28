3e214c26ed290bdb7945e8db09ae9ca673d675fc
Subin <omnc2019@gmail.com>
2025-11-28 10:48:04 -0500

    updated index routes

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,28 @@ import { Hono } from 'hono'
 import authController from './controllers/authController'
 import recipeController from './controllers/recipeController'
 
-const app = new Hono()
+type Bindings = {
+  FOODIST: D1Database
+  IMAGES: R2Bucket
+  JWT_SECRET: string
+}
 
+const app = new Hono<{ Bindings: Bindings }>()
+
+// Root route - health check
+app.get('/', (c) => {
+  return c.json({ 
+    message: 'Foodist API is running!',
+    version: '1.0.0',
+    endpoints: {
+      auth: '/auth',
+      recipes: '/recipes'
+    }
+  })
+})
+
+// Mount controllers
 app.route('/auth', authController)
-app.route('/recipe', recipeController)
+app.route('/recipes', recipeController)
 
 export default app
